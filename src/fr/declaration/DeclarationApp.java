1690044f3800225615992b1b7192a2fe12103585
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-10-04 14:22:27 +0200

    declaration et affichage

--- src/fr/declaration/DeclarationApp.java
+++ src/fr/declaration/DeclarationApp.java
@@ -2,6 +2,22 @@ package fr.declaration;
 
 public class DeclarationApp {
 	public static void main(String[] args) {
+		// déclarations
+		byte a = 13;
+		short b = 2;
+		int c = 25;
+		long d = 124664; 
+		float e = 1;
+		double f = 1.1;
+		char i = 'h';
+		boolean j = false;
+		String k = "Hello";
 		
+		// Affichges
+		System.out.println(k);
+		System.out.println(d);
+		System.out.println(a);
+
+
 	}
 }
